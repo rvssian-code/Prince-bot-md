9e0378320900f6c137b264f719cf7a280da08f2f
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2025-03-14 15:33:09 +0300

    Update Card.js

--- lazackcmds/Card.js
+++ lazackcmds/Card.js
@@ -1,10 +1,10 @@
 let handler = async m =>
   m.reply(
     `⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⣿⣿⣷⣜⢿⣧⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠻⣿⣿⣿⣿⣦⠄⠄
-乂───『 *DENZEL BOT*』───乂 
+乂───『 *MICKEY-TRONY BOT*』───乂 
 > Library : MICKEY INFO TECH
 > Version : 2.9.0
-> website : https://mickdadyhamza.websites.co.in/update/welcome-to-mickdady-hamza/2755144
+> website : https://rosy-cheetah-m2mt9z.mystrikingly.com/
 > source : https://github.com/Mickeymozy/Mickey-trony
 
       if you find error or want to upgrade to premium plan contact to the owner.
