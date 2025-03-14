997da664240d401d9db66850e513ea1271d02ff1
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2025-03-14 15:30:35 +0300

    Update pay.js

--- lazackcmds/pay.js
+++ lazackcmds/pay.js
@@ -3,7 +3,7 @@ import { join } from 'path'
 import axios from 'axios'
 
 let handler = async function (m, { conn, __dirname }) {
-  const githubRepoURL = 'https://github.com/Mickeymozy/Denzel-V2/'
+  const githubRepoURL = 'https://github.com/Mickeymozy/Mickey-trony'
 
   try {
     const [, username, repoName] = githubRepoURL.match(/github\.com\/([^/]+)\/([^/]+)/)
