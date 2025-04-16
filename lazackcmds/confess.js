d708212ddd730fc03c0de029239f9440b767ec61
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2025-04-16 20:48:45 +0300

    Update confess.js

--- lazackcmds/confess.js
+++ lazackcmds/confess.js
@@ -25,7 +25,7 @@ let handler = async (m, { conn, args, text, command }) => {
       forwardingScore: 999,
       isForwarded: true,
       forwardedNewsletterMessageInfo: {
-        newsletterJid: '120363200367779016@newsletter',
+        newsletterJid: '120363314875614529@newsletter',
         newsletterName: 'MICKDAD ',
         serverMessageId: 143,
       },
