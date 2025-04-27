d175cfa32612b7899e939417073b92e4774fe16e
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2025-04-27 18:59:45 +0300

    Update Card.js

--- lazackcmds/Card.js
+++ lazackcmds/Card.js
@@ -1,22 +1,28 @@
-let handler = async m =>
+let handler = async m => {
   m.reply(
-    `⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⣿⣿⣷⣜⢿⣧⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠻⣿⣿⣿⣿⣦⠄⠄
+    {
+      text: `⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⣿⣿⣷⣜⢿⣧⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠻⣿⣿⣿⣿⣦⠄⠄
 乂───『 *MICKEY-TRONY BOT*』───乂 
 > Library : MICKEY INFO TECH
 > Version : 2.9.0
 > website : https://rosy-cheetah-m2mt9z.mystrikingly.com/
 > source : https://github.com/Mickeymozy/Mickey-trony
 
-      if you find error or want to upgrade to premium plan contact to the owner.
+      If you find error or want to upgrade to premium plan contact to the owner.
       
 > Mickey-trony is bot made up by Mickey
-  __________________________
+  __________________________`,
+      buttons: [
+        { buttonId: 'sendText1', buttonText: { displayText: 'Send Text 1' }, type: 1 },
+        { buttonId: 'sendText2', buttonText: { displayText: 'Send Text 2' }, type: 1 }
+      ],
+      headerType: 1
+    }
+  );
+};
 
+handler.help = ['ruth'];
+handler.tags = ['main'];
+handler.command = ['card'];
 
-`.trim()
-  )
-handler.help = ['ruth']
-handler.tags = ['main']
-handler.command = ['card']
-
-export default handler
+export default handler;
