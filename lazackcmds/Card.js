2f7e227d232592431256778a9e77be80cc672b36
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2025-04-28 21:44:21 +0300

    Add files via upload

--- lazackcmds/Card.js
+++ lazackcmds/Card.js
@@ -1,20 +1,22 @@
-let handler = async m => {
+let handler = async m =>
   m.reply(
-    {
-      text: `⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⣿⣿⣷⣜⢿⣧⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠻⣿⣿⣿⣿⣦⠄⠄
+    `⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⣿⣿⣷⣜⢿⣧⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠻⣿⣿⣿⣿⣦⠄⠄
 乂───『 *MICKEY-TRONY BOT*』───乂 
 > Library : MICKEY INFO TECH
 > Version : 2.9.0
 > website : https://rosy-cheetah-m2mt9z.mystrikingly.com/
 > source : https://github.com/Mickeymozy/Mickey-trony
 
-      If you find error or want to upgrade to premium plan contact to the owner.
+      if you find error or want to upgrade to premium plan contact to the owner.
       
 > Mickey-trony is bot made up by Mickey
-  __________________________`,
-   
-handler.help = ['ruth'];
-handler.tags = ['main'];
-handler.command = ['card'];
+  __________________________
 
-export default handler;
+
+`.trim()
+  )
+handler.help = ['ruth']
+handler.tags = ['main']
+handler.command = ['card']
+
+export default handler
