a9b68dc958ca783e05dd563f620ef6e654ac7e6d
PRINCE TECH <princerwanda2003@gmail.com>
2025-05-04 00:32:55 +0300

    Update menu (1).js

--- lazackcmds/menu (1).js
+++ lazackcmds/menu (1).js
@@ -154,7 +154,7 @@ ${readMore}
             isForwarded: true,
             forwardingScore: 999,
             forwardedNewsletterMessageInfo: {
-              newsletterJid: '120363314875614529@newsletter', // Replace with actual channel JID
+              newsletterJid: '120363419996770740@newsletter', // Replace with actual channel JID
               newsletterName: 'PRINCETECH', // Replace with actual channel name
               serverMessageId: -1,
             },
