47decc096c3d18065440756b73c02f3298cb0b96
PRINCE TECH <princerwanda2003@gmail.com>
2025-05-03 17:49:50 +0300

    Update pay.js

--- lazackcmds/pay.js
+++ lazackcmds/pay.js
@@ -3,7 +3,7 @@ import { join } from 'path'
 import axios from 'axios'
 
 let handler = async function (m, { conn, __dirname }) {
-  const githubRepoURL = 'https://github.com/Mickeymozy/Mickey-trony'
+  const githubRepoURL = 'https://github.com/PRINCETECH20/Prince-bot-md'
 
   try {
     const [, username, repoName] = githubRepoURL.match(/github\.com\/([^/]+)\/([^/]+)/)
